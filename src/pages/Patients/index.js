1ad34869cd0f7e7fa6c997c0e4274780692b6ccb
Victor Reinor <victorreinor@gmail.com>
2020-05-04 19:38:03 -0300

    adiciona grid de alergias

--- src/pages/Patients/index.js
+++ src/pages/Patients/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-import { faTint, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faTint } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-
+import Add from '@material-ui/icons/Add';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import MaterialTable from 'material-table';
 import './styles.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,6 +27,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     paddingLeft: theme.spacing(6),
     paddingRight: theme.spacing(6),
+    height: '100%',
   },
   paper: {
     padding: theme.spacing(2),
@@ -21,16 +36,39 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Patients() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Alergia', field: 'alergia' },
+      { title: 'Categoria', field: 'categoria' },
+      { title: 'Gravidade', field: 'gravidade' },
+      { title: 'Reações', field: 'reacao' },
+      { title: 'Status Verificação', field: 'statusVerificacao' },
+    ],
+    data: [
+      {
+        alergia: 'PENICILINA G',
+        categoria: 'MEDICAMENTO',
+        gravidade: 'EXTREMA',
+        reacao: 'URTICARIA',
+        statusVerificacao: 'CONFIRMADO',
+      },
+      {
+        alergia: 'PENICILINA G',
+        categoria: 'MEDICAMENTO',
+        gravidade: 'EXTREMA',
+        reacao: 'URTICARIA',
+        statusVerificacao: 'EM ANALISE',
+      },
+      {
+        alergia: 'PENICILINA G',
+        categoria: 'MEDICAMENTO',
+        gravidade: 'EXTREMA',
+        reacao: 'URTICARIA',
+        statusVerificacao: 'CONFIRMADO',
+      },
+    ],
+  });
   const classes = useStyles();
-
-  function handleDatasheet() {
-    const el = document.getElementById('drawer-datasheet');
-    if (el.classList.contains('drawer-hide')) {
-      el.classList.remove('drawer-hide');
-    } else {
-      el.classList.add('drawer-hide');
-    }
-  }
 
   return (
     <div className={classes.root}>
@@ -98,7 +136,7 @@ function Patients() {
               <div
                 style={{
                   position: 'absolute',
-                  right: 76,
+                  right: 81,
                   width: 55,
                   height: 55,
                   backgroundColor: 'rgb(51, 51, 51)',
@@ -116,7 +154,7 @@ function Patients() {
             </div>
             <div className="drawer-patient">
               <div className="drawer-header">
-                <span className="title">CADASTRO DO PACIENTE</span>
+                <span className="title">FICHA DO CLIENTE</span>
               </div>
               <div id="drawer-datasheet" className="drawer-body">
                 <div className="drawer-datasheet-body">
@@ -134,13 +172,13 @@ function Patients() {
                 <div className="drawer-datasheet-body">
                   <span className="title-datasheet">CIDADE: </span>
                   <span className="subtitle-datasheet">
-                    Mirassol - São Paulo
+                    MIRASSOL - SÃO PAULO
                   </span>
                 </div>
                 <div className="drawer-datasheet-body">
                   <span className="title-datasheet">ENDERECO: </span>
                   <span className="subtitle-datasheet">
-                    Rua flores bela, 2729, centro
+                    RUA FLORES BELA, 2729, CENTRO
                   </span>
                 </div>
                 <div className="drawer-datasheet-body">
@@ -149,6 +187,76 @@ function Patients() {
                 </div>
               </div>
             </div>
+            <div className="drawer-patient">
+              <div className="drawer-header">
+                <span className="title">ALERGIAS</span>
+              </div>
+            </div>
+
+            <MaterialTable
+              style={{ marginTop: 15 }}
+              icons={{
+                Check,
+                DetailPanel: ChevronRight,
+                Delete: DeleteOutline,
+                Export: SaveAlt,
+                Filter: FilterList,
+                FirstPage,
+                LastPage,
+                NextPage: ChevronRight,
+                PreviousPage: ChevronLeft,
+                Search,
+                ThirdStateCheck: Remove,
+                Add,
+                SortArrow: ArrowDownward,
+                Clear: Add,
+                Edit,
+                ViewColumn,
+              }}
+              options={{
+                search: false,
+              }}
+              title=""
+              columns={state.columns}
+              data={state.data}
+              editable={{
+                onRowAdd: (newData) =>
+                  new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve();
+                      setState((prevState) => {
+                        const data = [...prevState.data];
+                        data.push(newData);
+                        return { ...prevState, data };
+                      });
+                    }, 600);
+                  }),
+                onRowUpdate: (newData, oldData) =>
+                  new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve();
+                      if (oldData) {
+                        setState((prevState) => {
+                          const data = [...prevState.data];
+                          data[data.indexOf(oldData)] = newData;
+                          return { ...prevState, data };
+                        });
+                      }
+                    }, 600);
+                  }),
+                onRowDelete: (oldData) =>
+                  new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve();
+                      setState((prevState) => {
+                        const data = [...prevState.data];
+                        data.splice(data.indexOf(oldData), 1);
+                        return { ...prevState, data };
+                      });
+                    }, 600);
+                  }),
+              }}
+            />
           </Paper>
         </Grid>
       </Grid>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Menu from '~/pages/_layouts/Menu';
-import Home from '~/pages/Home';
 import Patients from '~/pages/Patients';
 
 function Routes() {
   return (
     <Switch>
       <Menu>
-        <Route path="/" exact component={Home} />
-        <Route path="/patients" exact component={Patients} />
+        <Route path="/" exact component={Patients} />
       </Menu>
     </Switch>
   );
